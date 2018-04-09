737dc42ae5560fb524dcd9400a3d1ea208996c26
Bryan Long <BryanLong1@gmail.com>
2018-04-09 13:51:31 -0600

    Adding layers to map

--- src/components/BaseMap.vue
+++ src/components/BaseMap.vue
@@ -1,13 +1,29 @@
 <template>
-   <div class=" row ">
-      <div class="col-md-10 ">
-        <div id="map" class="map"></div>
-        <!-- Denver Open Data Map will be rendered here -->
-      </div>
-      <div class="col-md-2 ">
-        <!-- The layer checkboxes will be rendered here -->
-      </div>
+  <div class=" row ">
+    <div class="col-md-9">
+    <div id="map" class="map">  
     </div>
+    <!-- Denver Open Data Map will be rendered here -->
+    </div>
+    <div class="col-md-3">
+     <!-- The layer checkboxes will be rendered here -->
+    <div
+    class="form-check"
+    v-for="layer in layers"
+    :key="layer.id"
+    >
+    <label class="form-check-label">
+    <input
+    class="form-check-input"
+    type="checkbox"
+    v-model="layer.active"
+    @change="layerChanged(layer.id, layer.active)"
+    />
+    {{ layer.name }}
+    </label>
+    </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -25,12 +41,60 @@ export default {
         name: "Crime",
         active: false,
         features: [
-          // import crome data as an object here. EX: {
-          //   id: 0,
-          //   name: 'Bogart\'s Smokehouse',
-          //   type: 'marker',
-          //   coords: [38.6109607, -90.2050322],
-          // },
+          {
+            id: "20181388",
+            name: "auto-theft",
+            type: "marker",
+            coords: [39.7770144, -104.8046632]
+          },
+          {
+            id: "20181016",
+            name: "auto-theft",
+            type: "marker",
+            coords: [39.7633303, -104.9305771]
+          },
+          {
+            id: "20181498",
+            name: "larceny",
+            type: "marker",
+            coords: [39.739432, -104.9631267]
+          },
+          {
+            id: "20181388",
+            name: "auto-theft",
+            type: "marker",
+            coords: [39.7770144, -104.8046632]
+          },
+          {
+            id: "20181016",
+            name: "auto-theft",
+            type: "marker",
+            coords: [39.7633303, -104.9305771]
+          },
+          {
+            id: "20181498",
+            name: "larceny",
+            type: "marker",
+            coords: [39.739432, -104.9631267]
+          },
+          {
+            id: "20181388",
+            name: "auto-theft",
+            type: "marker",
+            coords: [39.7771134, -104.8046432]
+          },
+          {
+            id: "20181016",
+            name: "auto-theft",
+            type: "marker",
+            coords: [39.7633203, -104.9305671]
+          },
+          {
+            id: "20181498",
+            name: "larceny",
+            type: "marker",
+            coords: [39.739332, -104.963147]
+          }
         ]
       }
     ]
@@ -42,16 +106,36 @@ export default {
   methods: {
     initMap() {
       this.map = L.map("map").setView([39.7392, -104.9903], 14);
-      this.tileLayer = L.tileLayer("https://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}", {
-        attribution:
-          'Imagery from <a href="http://giscience.uni-hd.de/">GIS</a> Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-        maxZoom: 20,
-        attribution:
-          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
-      });
+      this.tileLayer = L.tileLayer(
+        "https://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}",
+        {
+          attribution:
+            'Imagery from <a href="http://giscience.uni-hd.de/">GIS</a> Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+          maxZoom: 20,
+          attribution:
+            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
+        }
+      );
       this.tileLayer.addTo(this.map);
     },
-    initLayers() {}
+    initLayers() {
+      this.layers.forEach(layer => {
+        const markerFeature = layer.features.filter(feature => feature.type === "marker");
+      });
+      markerFeature.forEach(feature => {
+        feature.leafletObject = L.marker(feature.coords).bindPopup(feature.name);
+      });
+    },
+    layerChanged(layerId, active) {
+      const layer = this.layers.find(layer => layer.id === layerId);
+      layer.features.forEach(feature => {
+        if (active) {
+          feature.leafletObject.addTo(this.map);
+        } else {
+          feature.leafletObject.removerFrom(this.map);
+        }
+      });
+    }
   }
 };
 </script>
@@ -59,6 +143,6 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .map {
-  height: 90vh;
+  height: 100vh;
 }
 </style>
